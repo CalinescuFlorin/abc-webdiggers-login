d90f28c450ff3160112d60b86c95d7acb53efda4
CalinescuFlorin <aflorin149@yahoo.com>
2024-08-08 14:59:08 +0300

    test: authentication

--- src/test/java/web_diggers/abc_backend/AbcBackendApplicationTests.java
+++ src/test/java/web_diggers/abc_backend/AbcBackendApplicationTests.java
@@ -1,24 +1,201 @@
 package web_diggers.abc_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import web_diggers.abc_backend.security.auth.AuthenticationController;
+import web_diggers.abc_backend.security.auth.model.AuthenticationRequest;
+import web_diggers.abc_backend.security.auth.model.AuthenticationResponse;
+import web_diggers.abc_backend.security.auth.model.RegisterRequest;
+import web_diggers.abc_backend.security.user.UserController;
+import web_diggers.abc_backend.security.user.UserRepository;
+import web_diggers.abc_backend.security.user.model.User;
+
+import java.util.Date;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
 class AbcBackendApplicationTests {
-	String test = "test";
+	@Autowired
+	private AuthenticationController authenticationController;
+	@Autowired
+	private UserController userController;
+	@Autowired
+	private UserRepository userRepository;
+
+	private final String siteLink = "http://localhost:8080/api/v1/";
+	private final TestRestTemplate restTemplate = new TestRestTemplate();
+
+
 	@Test
 	void contextLoads(){
-		assertThat(test).isNotNull();
+		assertThat(authenticationController).isNotNull();
+		assertThat(userController).isNotNull();
+		assertThat(siteLink).isEqualTo("http://localhost:8080/api/v1/");
 	}
 
 	@Test
 	void runTests(){
-		simpleTest();
+		authenticationTests();
+		authorizationTests();
 	}
 
-	void simpleTest(){
-		assertThat(test).isEqualTo("test");
+	void authenticationTests(){
+		registerShouldCreateAccount();
+		registerWithUsedAddressShouldThrowError();
+		loginWithExistingAccountIsSuccessful();
+		loginWithNonExistingAccountIsUnsuccessful();
+		loginWithIncorrectCredentialsIsUnsuccessful();
+		cleanAuthTests();
+	}
+
+	void authorizationTests(){
+
+	}
+
+	void registerShouldCreateAccount(){
+		RegisterRequest request = RegisterRequest.builder()
+				.email("auth_test_account@abc_test.test")
+				.password("auth_test")
+				.firstName("auth")
+				.lastName("test")
+				.build();
+
+		// Send register request to endpoint
+		ParameterizedTypeReference<AuthenticationResponse> responseType = new ParameterizedTypeReference<AuthenticationResponse>() { };
+		ResponseEntity<AuthenticationResponse> result = restTemplate.exchange(
+				siteLink + "auth/register", HttpMethod.POST,
+				new HttpEntity<>(request), responseType);
+
+		// Verify response
+		AuthenticationResponse response = result.getBody();
+		assertThat(response).isNotNull();
+
+		assertThat(response.getStatus()).isEqualTo("success");
+		assertThat(response.getMessage()).isEqualTo("Account created.");
+		assertThat(response.getRole()).isEqualTo("USER");
+		assertThat(response.getFirstName()).isEqualTo("auth");
+		assertThat(response.getLastName()).isEqualTo("test");
+		assertThat(response.getToken()).isNotNull();
+
+		// Verify that user was added into the database
+		Optional<User> query = userRepository.findUserByEmail("auth_test_account@abc_test.test");
+		assertThat(query).isPresent();
+		User testUser = query.get();
+
+		assertThat(testUser.getRole().getValue()).isEqualTo("ROLE_USER");
+		assertThat(testUser.getFirstName()).isEqualTo("auth");
+		assertThat(testUser.getLastName()).isEqualTo("test");
+		assertThat(testUser.getPassword()).isNotNull();
+	}
+
+	void registerWithUsedAddressShouldThrowError(){
+		RegisterRequest request = RegisterRequest.builder()
+				.email("auth_test_account@abc_test.test")
+				.password("auth_test2")
+				.firstName("auth2")
+				.lastName("test2")
+				.build();
+
+		// Send register request to endpoint
+		ParameterizedTypeReference<AuthenticationResponse> responseType = new ParameterizedTypeReference<AuthenticationResponse>() { };
+		ResponseEntity<AuthenticationResponse> result = restTemplate.exchange(
+				siteLink + "auth/register", HttpMethod.POST,
+				new HttpEntity<>(request), responseType);
+
+		// Verify response
+		AuthenticationResponse response = result.getBody();
+		assertThat(response).isNotNull();
+
+		assertThat(response.getStatus()).isEqualTo("fail");
+		assertThat(response.getMessage()).isEqualTo("Email address is already taken.");
+		assertThat(response.getRole()).isEqualTo("");
+		assertThat(response.getFirstName()).isEqualTo("");
+		assertThat(response.getFirstName()).isEqualTo("");
+		assertThat(response.getToken()).isEqualTo("");
+	}
+
+	void loginWithExistingAccountIsSuccessful(){
+		AuthenticationRequest request = AuthenticationRequest.builder()
+				.email("auth_test_account@abc_test.test")
+				.password("auth_test")
+				.build();
+
+		// Send login request to endpoint
+		ParameterizedTypeReference<AuthenticationResponse> responseType = new ParameterizedTypeReference<AuthenticationResponse>() { };
+		ResponseEntity<AuthenticationResponse> result = restTemplate.exchange(
+				siteLink + "auth/authenticate", HttpMethod.POST,
+				new HttpEntity<>(request), responseType);
+
+		// Verify response
+		AuthenticationResponse response = result.getBody();
+		assertThat(response).isNotNull();
+
+		assertThat(response.getStatus()).isEqualTo("success");
+		assertThat(response.getMessage()).isEqualTo("Login successful.");
+		assertThat(response.getRole()).isEqualTo("USER");
+		assertThat(response.getFirstName()).isEqualTo("auth");
+		assertThat(response.getLastName()).isEqualTo("test");
+		assertThat(response.getToken()).isNotNull();
+	}
+
+	void loginWithNonExistingAccountIsUnsuccessful(){
+		AuthenticationRequest request = AuthenticationRequest.builder()
+				.email("fictional_auth_test_account@abc_test.test")
+				.password("auth_test")
+				.build();
+
+		// Send login request to endpoint
+		ParameterizedTypeReference<AuthenticationResponse> responseType = new ParameterizedTypeReference<AuthenticationResponse>() { };
+		ResponseEntity<AuthenticationResponse> result = restTemplate.exchange(
+				siteLink + "auth/authenticate", HttpMethod.POST,
+				new HttpEntity<>(request), responseType);
+
+		// Verify response
+		AuthenticationResponse response = result.getBody();
+		assertThat(response).isNotNull();
+
+		assertThat(response.getStatus()).isEqualTo("fail");
+		assertThat(response.getMessage()).isEqualTo("Bad credentials");
+		assertThat(response.getRole()).isEqualTo("");
+		assertThat(response.getFirstName()).isEqualTo("");
+		assertThat(response.getLastName()).isEqualTo("");
+		assertThat(response.getToken()).isEqualTo("");
+	}
+
+	void loginWithIncorrectCredentialsIsUnsuccessful(){
+		AuthenticationRequest request = AuthenticationRequest.builder()
+				.email("auth_test_account@abc_test.test")
+				.password("auth_test_wrong")
+				.build();
+
+		// Send login request with wrong password to endpoint
+		ParameterizedTypeReference<AuthenticationResponse> responseType = new ParameterizedTypeReference<AuthenticationResponse>() { };
+		ResponseEntity<AuthenticationResponse> result = restTemplate.exchange(
+				siteLink + "auth/authenticate", HttpMethod.POST,
+				new HttpEntity<>(request), responseType);
+
+		// Verify response
+		AuthenticationResponse response = result.getBody();
+		assertThat(response).isNotNull();
+
+		assertThat(response.getStatus()).isEqualTo("fail");
+		assertThat(response.getMessage()).isEqualTo("Bad credentials");
+		assertThat(response.getRole()).isEqualTo("");
+		assertThat(response.getFirstName()).isEqualTo("");
+		assertThat(response.getLastName()).isEqualTo("");
+		assertThat(response.getToken()).isEqualTo("");
+	}
+
+	void cleanAuthTests(){
+		userRepository.deleteUserByEmail("auth_test_account@abc_test.test");
 	}
 }

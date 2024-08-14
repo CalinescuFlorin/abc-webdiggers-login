cead4c465ec30b843496bebe0a65ca4e8727ca2d
Aleb1111 <115371367+Aleb1111@users.noreply.github.com>
2024-08-14 12:57:58 +0300

    VM Configs for keys and email

--- src/main/java/web_diggers/abc_backend/security/email/EmailSenderService.java
+++ src/main/java/web_diggers/abc_backend/security/email/EmailSenderService.java
@@ -1,6 +1,7 @@
 package web_diggers.abc_backend.security.email;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,12 +11,15 @@ public class EmailSenderService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String email;
+
     public void sendMail(String address,
                          String subject,
                          String body){
 
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("abc.backend.t1@gmail.com\n");
+        message.setFrom(email);
         message.setTo(address);
         message.setText(body);
         message.setSubject(subject);

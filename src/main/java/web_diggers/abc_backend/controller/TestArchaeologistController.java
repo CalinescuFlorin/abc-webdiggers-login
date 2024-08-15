e3a9acbdb9b2ed664392591e7336389315224fe4
CalinescuFlorin <florin.calinescu@stud.ubbcluj.ro>
2024-08-15 13:57:19 +0300

    fix: add missing controllers

---
+++ src/main/java/web_diggers/abc_backend/controller/TestArchaeologistController.java
@@ -0,0 +1,17 @@
+package web_diggers.abc_backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/arheo")
+@RequiredArgsConstructor
+public class TestArchaeologistController {
+    @RequestMapping
+    public ResponseEntity<String> testController(){
+        return new ResponseEntity<>("User has access to this resource.", HttpStatus.OK);
+    }
+}
